878d19efd9af193042ff8cebe6898e5e104531b1
Jordi Smit <j0rd1smit@users.noreply.github.com>
2023-03-23 14:39:11 +0100

    setup javascript build

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,14 +1,16 @@
-jQuery(document).ready(function ($) {
+import $ from "jquery";
+
+$(document).ready(function ($) {
   var offset = 1250;
   var duration = 800;
-  jQuery(window).scroll(function () {
-    if (jQuery(this).scrollTop() > offset) {
-      jQuery(".back-to-top").fadeIn(duration);
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > offset) {
+      $(".back-to-top").fadeIn(duration);
     } else {
-      jQuery(".back-to-top").fadeOut(duration);
+      $(".back-to-top").fadeOut(duration);
     }
   });
-  jQuery(".back-to-top").click(function (event) {
+  $(".back-to-top").click(function (event) {
     event.preventDefault();
     jQuery("html, body").animate({ scrollTop: 0 }, duration);
     return false;
@@ -62,7 +64,7 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  // Hide Header on on scroll down
+  // Hide Header on scroll down
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
